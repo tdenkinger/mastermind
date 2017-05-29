3a37e6216a301294747d5660dbf10750d303198b
Troy Denkinger <tdenkinger@gmail.com>
2017-05-29 12:41:49 -0400

    Initial commit.

---
+++ lib/mastermind.ex
@@ -0,0 +1,54 @@
+defmodule Mastermind do
+  @moduledoc """
+  Create a "code" using 4 colored pegs
+  Player guesses the colors and positions
+
+  Each white peg means that one of the guessed pegs is correct but is in the wrong hole.
+  Each black peg means that one of the guessed pegs is correct and is in the right hole.
+  The order of the white and black pegs does not matter.
+
+  pegs colors: green, red, blue, yellow, purple, orange
+  """
+
+  @doc """
+  Accepts a guess and a code of color atoms.
+
+  Returns `[:solved]` if the guess and the code are the same
+  Otherwise returns the hint pegs, shuffled, as a list of atoms
+  """
+  def guess(%{guess: guess, code: code}) when guess == code do
+    [:solved]
+  end
+
+  def guess(%{guess: guess, code: code}) do
+    set_hints(guess, code)
+    |> Enum.shuffle
+  end
+
+  defp set_hints(guess, code) do
+    set_hints(:black, guess, code) ++ set_hints(:white, guess, code)
+  end
+
+  defp set_hints(:black, guess, code) do
+    Enum.zip(guess, code)
+    |> Enum.filter(fn(pegs) -> elem(pegs, 0) == elem(pegs, 1) end)
+    |> Enum.reduce([], fn(_, hints) -> [:black | hints] end)
+  end
+
+  defp set_hints(:white, guess, code) do
+    Enum.zip(guess, code)
+    |> Enum.filter(fn(pegs) -> elem(pegs, 0) != elem(pegs, 1) end)
+    |> Enum.unzip
+    |> get_white_hints
+  end
+
+  defp get_white_hints({guess, code}) do
+    guess
+    |> Enum.reduce([], fn(peg, hints) ->
+      case Enum.member?(code, peg) do
+        :true -> [:white | hints]
+            _ -> hints
+      end
+    end)
+  end
+end
---
+++ test/mastermind_test.exs
@@ -0,0 +1,39 @@
+defmodule MastermindTest do
+  use ExUnit.Case
+  doctest Mastermind
+
+  test "declares a winner when guess and code match" do
+    result = Mastermind.guess(%{guess: [:red, :red, :red, :red],
+                                code:  [:red, :red, :red, :red]})
+
+    assert result == [:solved]
+  end
+
+  test "hints are empty when there are no correct guesses" do
+    result = Mastermind.guess(%{guess: [:red, :red,  :blue,  :blue],
+                                code:  [:green, :green, :orange, :orange]})
+
+    assert result == []
+  end
+
+  test "hints have only white pegs when colors are correct, position is not" do
+    result = Mastermind.guess(%{guess: [:red, :red,  :blue,  :blue],
+                                code:  [:green, :green, :red, :red]})
+
+    assert result == [:white, :white]
+  end
+
+  test "hints have only black pegs when positions are correct" do
+    result = Mastermind.guess(%{guess: [:red, :red, :blue, :blue],
+                                code:  [:red, :red, :red, :blue]})
+
+    assert result == [:black, :black, :black]
+  end
+
+  test "hints can have both black and white pegs" do
+    result = Mastermind.guess(%{guess: [:red, :red,  :blue,  :purple],
+                                code:  [:red, :blue, :green, :purple]})
+
+    assert Enum.sort(result) == [:black, :black, :white]
+  end
+end
