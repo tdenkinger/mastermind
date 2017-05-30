ddd4cfce0fe500cb3d6086f46cc0ac4fb81b785d
Troy Denkinger <tdenkinger@gmail.com>
2017-05-29 22:38:54 -0400

    Change the game shell to handle the new tuple-based returns from the guess evaluator.

--- lib/mastermind.ex
+++ lib/mastermind.ex
@@ -1,23 +1,31 @@
 defmodule Mastermind do
   def start do
     code = Mastermind.CreateCode.new
+    IO.inspect code
     run(%{code: code, turns: []})
   end
 
   def run(%{code: code, turns: turns}) do
-    turns
-    |> Enum.reverse
-    |> IO.inspect
-
+    display_prior_turns(turns)
     guess = get_player_guess()
 
     case Mastermind.GuessEvaluator.guess(%{guess: guess, code: code}) do
-      [:solved] -> "Winner"
-      hint      -> run(%{code: code,
-                         turns: [%{hint: hint, guess: guess} | turns]})
+      {:ok, "solved"} -> "Winner"
+      {:ok,     hint} -> run(%{code: code, turns: [%{hint: hint, guess: guess} | turns]})
+                    _ -> "Error"
     end
   end
 
+  def display_prior_turns(turns) do
+    turns
+    |> Enum.reverse
+    |> Enum.each(fn(turn) -> print_prior_guess(turn) end)
+  end
+
+  def print_prior_guess(turn) do
+    Enum.join(turn.guess, " ") <> " | " <> Enum.join(turn.hint, "/")
+    |> IO.puts
+  end
   def get_player_guess do
     IO.gets("Your guess: ")
     |> String.split
