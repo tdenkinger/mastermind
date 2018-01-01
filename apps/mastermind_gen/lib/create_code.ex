2380189f3ba99607de8eb18d7735adbaa6787c46
Troy Denkinger <tdenkinger@gmail.com>
2018-01-01 14:03:49 -0500

    Remove the gen_server application to its own repo, remove the umbrella app, and pull the pure functional application to the top-level.

--- apps/mastermind_gen/lib/create_code.ex
+++
@@ -1,8 +0,0 @@
-defmodule MastermindGen.CreateCode  do
-  @code_pegs [:green, :red, :blue, :yellow, :purple, :orange]
-
-  def new(count \\ 4) do
-    Range.new(1, count)
-    |> Enum.reduce([], fn(_, code) -> [Enum.random(@code_pegs) | code] end)
-  end
-end
